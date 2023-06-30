a70114f6e38e58b0a62112bcd77e81b939e6a222
Jaidenver <jaiderhurtado271@gmail.com>
2023-06-30 14:48:08 -0500

    Retomando spring boot

---
+++ src/test/java/com/funcion/Original/OriginalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funcion.Original;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OriginalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
